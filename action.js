67a1868091b822b229db0b06f98abff6cc081567
jhanstra <jared@coprime.io>
2023-07-24 18:39:14 -0600

    new version

--- action.js
+++ action.js
@@ -230,8 +230,8 @@ const waitForDeploymentToStart = async ({
           "Authorization": `Bearer ${VERCEL_TOKEN}`
         }
       });
-      console.log('vercel projects', vercelProjects.data)
-      const hi = vercelProjects.data.map(p => ({ name: p.name, env: p.env, link: p.link, latest: p.latestDeployments, targets: p.targets }))
+      // console.log('vercel projects', vercelProjects.data)
+      const hi = vercelProjects.data.projects.map(p => ({ name: p.name, env: p.env, link: p.link, latest: p.latestDeployments, targets: p.targets }))
       console.log('hi', JSON.stringify(hi))
       if (!hasQueuedDeployments) return vercelDeps.data.deployments.filter(d => d.state !== 'CANCELED').map(d => d.url)
 
