90277a47eff50c23d92534cca368642ad3735d2f
jhanstra <jared@coprime.io>
2023-07-24 19:07:45 -0600

    new version

--- action.js
+++ action.js
@@ -232,13 +232,14 @@ const waitForDeploymentToStart = async ({
       });
       // console.log('vercel projects', vercelProjects.data)
       const hi = vercelProjects.data.projects.map(p => ({ name: p.name, env: p.env, link: p.link, latest: p.latestDeployments, targets: p.targets }))
-      const urls = vercelProjects.data.projects.map(d => d.latestDeployments)
+      const latestDeployments = vercelProjects.data.projects.map(d => d.latestDeployments)
       const targets = vercelProjects.data.projects.map(d => d.targets)
       const links = vercelProjects.data.projects.map(d => d.targets)
+      const latestLinks = latestDeployments.map(project => project.map(d => ({ url: d.url, name: d.name })))
       // console.log('urls', urls)
       console.log('stringified urls', JSON.stringify(urls, null, 2))
       console.log('targets', JSON.stringify(targets, null, 2))
-      console.log('links', JSON.stringify(links, null, 2))
+      console.log('latestlinks', JSON.stringify(latestLinks, null, 2))
       if (!hasQueuedDeployments) return vercelDeps.data.deployments.filter(d => d.state !== 'CANCELED').map(d => d.url)
 
       // return vercelDeps.data.deployments;
